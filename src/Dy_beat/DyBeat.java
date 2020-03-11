137910a4d7c1fff40176aa006303872e64cbfdc0
xohoon <xohoonx2@gmail.com>
2020-03-11 19:09:11 +0900

    C language base beat game project

---
+++ src/Dy_beat/DyBeat.java
@@ -0,0 +1,16 @@
+package Dy_beat;
+
+import javax.swing.JFrame;
+
+public class DyBeat extends JFrame{
+	
+	public DyBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
