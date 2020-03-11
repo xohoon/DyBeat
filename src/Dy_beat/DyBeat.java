054c72274ffc939d6e83c4c7ae3489235866cd4e
xohoon <xohoonx2@gmail.com>
2020-03-11 19:41:19 +0900

    background img  add

--- src/Dy_beat/DyBeat.java
+++ src/Dy_beat/DyBeat.java
@@ -1,8 +1,17 @@
 package Dy_beat;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DyBeat extends JFrame{
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image mainBackground;
 	
 	public DyBeat() {
 		setTitle("Dynamic Beat");
@@ -11,6 +20,21 @@ public class DyBeat extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		mainBackground = new ImageIcon(Main.class.getResource("../imgs/MainBg.jpg")).getImage();
 	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(mainBackground, 0, 0, null);
+		this.repaint();
+	}
+	
 
 }
