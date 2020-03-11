71b8b98717c43928e47b250b36bc961c8358d970
xohoon <xohoonx2@gmail.com>
2020-03-11 23:28:17 +0900

    music title, sub image update

--- src/Dy_beat/DyBeat.java
+++ src/Dy_beat/DyBeat.java
@@ -15,26 +15,46 @@ import javax.swing.JLabel;
 
 public class DyBeat extends JFrame{
 	
-	private Image screenImage;
 	private Graphics screenGraphic;
+	private Image screenImage;
 	
 	private Image background = new ImageIcon(Main.class.getResource("../imgs/introBg.jpg")).getImage();;
+	// title img
+	private Image titleImage = new ImageIcon(Main.class.getResource("../imgs/poem.png")).getImage();
+	private Image selectedImage = new ImageIcon(Main.class.getResource("../imgs/i1.png")).getImage();;
+	
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../imgs/manuBar.png")));
 	
+	// exit button
 	private ImageIcon exitButtonEnterImage = new ImageIcon(Main.class.getResource("../imgs/closeButtonEnter.png"));;
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../imgs/closeButtonBasic.png"));;
 	
+	// start button
 	private ImageIcon startButtonEnterImage = new ImageIcon(Main.class.getResource("../imgs/startEnter.png"));;
 	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../imgs/startBasic.png"));;
+
+	// left button
+	private ImageIcon leftButtonEnterImage = new ImageIcon(Main.class.getResource("../imgs/leftEnter.png"));;
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../imgs/leftBasic.png"));;
 	
+	// right button
+	private ImageIcon rightButtonEnterImage = new ImageIcon(Main.class.getResource("../imgs/rightEnter.png"));;
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../imgs/rightBasic.png"));;
+	
+	// quit button
 	private ImageIcon quitButtonEnterImage = new ImageIcon(Main.class.getResource("../imgs/quitEnter.png"));;
 	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../imgs/quitBasic.png"));;
 
+	
 	private JButton exitButton = new JButton(exitButtonBasicImage);
 	private JButton startButton = new JButton(startButtonBasicImage);
 	private JButton quitButton = new JButton(quitButtonBasicImage);
+	private JButton leftButton = new JButton(leftButtonBasicImage);
+	private JButton rightButton = new JButton(rightButtonBasicImage);
 	
 	private int mouseX, mouseY;
+	
+	private boolean isMainScreen = false;
 	
 	public DyBeat() {
 		setUndecorated(true);
@@ -90,7 +110,7 @@ public class DyBeat extends JFrame{
 			public void mouseEntered(MouseEvent e) {
 				startButton.setIcon(startButtonEnterImage);
 				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-				Music buttonEnteredMusic = new Music("bbb.mp3", false);
+				Music buttonEnteredMusic = new Music("iuo.mp3", false);
 				buttonEnteredMusic.start();
 			}
 			@Override
@@ -100,12 +120,15 @@ public class DyBeat extends JFrame{
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
-				Music buttonPressedMusic = new Music("bbb.mp3", false);
+				Music buttonPressedMusic = new Music("iuo.mp3", false);
 				buttonPressedMusic.start();
 				// game start event
 				startButton.setVisible(false);
 				quitButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
 				background = new ImageIcon(Main.class.getResource("../imgs/gameBackground.jpg")).getImage();
+				isMainScreen = true;
 			}
 		});
 		add(startButton);
@@ -121,7 +144,7 @@ public class DyBeat extends JFrame{
 			public void mouseEntered(MouseEvent e) {
 				quitButton.setIcon(quitButtonEnterImage);
 				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-				Music buttonEnteredMusic = new Music("bbb.mp3", false);
+				Music buttonEnteredMusic = new Music("iuo.mp3", false);
 				buttonEnteredMusic.start();
 			}
 			@Override
@@ -144,6 +167,64 @@ public class DyBeat extends JFrame{
 		add(quitButton);
 		// quit button //
 		
+		// left button //
+		leftButton.setVisible(false);
+		leftButton.setBounds(140, 330, 64, 64);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnterImage);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("iuo.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonPressedMusic = new Music("close.mp3", false);
+				buttonPressedMusic.start();
+				// left button event
+			}
+		});
+		add(leftButton);
+		// left button //
+	
+		// right button //
+		rightButton.setVisible(false);
+		rightButton.setBounds(1080, 330, 64, 64);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnterImage);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("iuo.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonPressedMusic = new Music("close.mp3", false);
+				buttonPressedMusic.start();
+				// right button event
+			}
+		});
+		add(rightButton);
+		// right button //
+				
 		menuBar.setBounds(0, 0, 1280, 30);
 		menuBar.addMouseListener(new MouseAdapter() {
 			@Override
@@ -175,6 +256,10 @@ public class DyBeat extends JFrame{
 	
 	public void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null);
+		if(isMainScreen) {
+			g.drawImage(selectedImage, 340, 100, null);
+			g.drawImage(titleImage, 340, 70, null);
+		}
 		paintComponents(g);
 		this.repaint();
 	}
